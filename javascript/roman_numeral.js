837f438668615f4bfc184869352eaeae6a12213b
Thomas Fowler <thomas.fowler@flatironschool.com>
2021-09-23 15:40:56 -0600

    Initial solution.

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,14 @@
-function romanNumeral(string) {
-  // type your code here
+const romanNumeral = (string) => {
+  const numeralMap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+  console.log(string)
+  const numerals = string.split('')
+  return numerals.reduce((p, c, i, arr) => {
+    const curVal = numeralMap[c.toUpperCase()]
+    if (i === 0) return curVal 
+    const preVal = numeralMap[arr[i - 1]]
+    const tempVal = preVal >= curVal ? p + curVal : (p - preVal) + (curVal - preVal)
+    return tempVal 
+  }, 0)
 }
 
 if (require.main === module) {
@@ -16,6 +25,11 @@ if (require.main === module) {
 
   console.log("Expecting: 402");
   console.log(romanNumeral('CDII'));
+
+  console.log("");
+
+  console.log("Expecting: 3848");
+  console.log(romanNumeral('MMMDCCCXLVIII'));
 }
 
 module.exports = romanNumeral;
